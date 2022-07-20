1a088a69946d5ab5068058c757afc89b41a0a460
AminatAdeshewa <adeshewaayoni@gmail.com>
2022-07-20 17:19:45 +0100

    Pulling development

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -7,7 +7,7 @@
     />
   </div>
   <div class="flex">
-    <div><sideBar /></div>
+    <div><sideBar /></div>  
     <div class=" ml-6"><router-view /></div>
     <!-- <div class="ml-6"><item/></div> -->
     <!-- <div><Users /></div> -->
